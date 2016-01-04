172f267a180818f107da173ba30059f8971e3efe
rokerTT <bessonnyiserega@yandex.ru>
2016-01-04 23:29:06 +0300

    user input

--- Calculator.java
+++ Calculator.java
@@ -1,29 +1,39 @@
 /**
-    Класс реализует калькулятор */
-public class Calculator{
-/**
- * Реализует вычисления*/
+ * Класс выполняет вычисления
+ */
+
+public class Calculator {
     private int result;
 
-    /**Суммирует аргументы
-     * @param params Аргументы вычисления
-     */
-    public void add (int...params){
-        for (Integer param:params){
-            this.result+=param;
+    public Calculator() {}
+
+    public void add(int par1, int par2) {/**Метод выполняет сложение*/
+       par1 += par2;
+       this.result = par1;
+    }
+
+    public void sub(int par1, int par2) {/**Метод выполняет вычетание*/
+        par1 += -par2;
+        this.result = par1;
+    }
+
+    public void mult(int par1, int par2) {/**Метод выполняет умножение*/
+        par1 *= par2;
+        this.result = par1;
+    }
+
+    public void div(int par1, int par2) {/**Метод выполняет деление*/
+        if(par2 != 0) {
+            par1 /= par2;
+            this.result = par1;
         }
     }
-    /**
-     Получить результат
-     @return результат суммирования
-     */
-    public int getResult(){
+
+    public int getResult() {/**Метод возвращает результат операции*/
         return this.result;
     }
-/** Очистть результат вычисления*/
-    public void cleanResult(){
-        this.result=0;
+
+    public void cleanResult() {/**Метод очищает выполнение операций*/
+        this.result = 0;
     }
-
-
 }
--- InteractRunner.java
+++ InteractRunner.java
@@ -9,10 +9,30 @@ public class InteractRunner{
             while (!exit.equals("yes")){
                 System.out.println("Enter first argument: ");
                 String first=reader.next();
+                System.out.println("Change operator: + , - , * or /");
+                String operator=reader.next();
                 System.out.println("Enter second argument: ");
                 String second=reader.next();
-                calc.add(Integer.valueOf(first),Integer.valueOf(second));
-                System.out.println("Result: "+calc.getResult());
+                switch (operator){
+                    case "+":
+                        calc.add(Integer.valueOf(first), Integer.valueOf(second));
+                        System.out.println("Summ: "+first+" + "+second+" = "+calc.getResult());
+                        break;
+                    case "-":
+                        calc.sub(Integer.valueOf(first), Integer.valueOf(second));
+                        System.out.println("Cut: "+first+" - "+second+" = "+calc.getResult());
+                        break;
+                    case "*":
+                        calc.mult(Integer.valueOf(first), Integer.valueOf(second));
+                        System.out.println("Mult: "+first+" * "+second+" = "+calc.getResult());
+                        break;
+                    case "/":
+                        calc.div(Integer.valueOf(first), Integer.valueOf(second));
+                        System.out.println("Div: "+first+" / "+second+" = "+calc.getResult());
+                        break;
+                    default: System.out.println("Command ERROR!!!");
+                        break;
+                }
                 calc.cleanResult();
                 System.out.println("Exit: + yes/no");
                 exit = reader.next();
